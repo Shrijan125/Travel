db92ec4ae32fe295e0f908251c6f9be736aa7875
Shrijan <shrijanshreshth3@gmail.com>
2024-12-15 15:43:28 +0530

    Initialised Backend

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import apiV1Router from './routes/route.js';
+
+const app = express();
+
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(express.static("public"));
+
+app.use("/api/v1", apiV1Router);
+
+export { app };
